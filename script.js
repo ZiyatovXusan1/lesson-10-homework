a20ee395681c72f8340f2375ecb7e499b067c464
ZiyatovXusan1 <xusan0083@gmail.com>
2025-08-24 09:55:00 +0500

    massives

---
+++ script.js
@@ -0,0 +1,25 @@
+// ! 1-masala
+// function getOddDividersSum(n) {
+//     let sum = 0;
+//     for (let i = 0; i < n; i++) {
+//         if(n % i === 0 && i % 2 !== 0) {
+//             sum += i;
+//         }
+//     }
+//     return sum;
+// }
+// let n = 15;
+// console.log(getOddDividersSum(n));
+
+// ! 2-masala
+// function search (arr, str) {
+//     let result = arr.filter(names => names.toLowerCase().includes(str.toLowerCase()));
+
+//     return result;
+// }
+// let arr = ['Aziz', 'Xusan', 'Muhammad', 'Xasan'];
+// let str = 'X';
+// console.log(search(arr, str));
+
+// ! 3-masala
+
